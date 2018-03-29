5b6fec61f37f2c61d7912452e09ea1463c095851
luizhenriquefbb <luizhenriquefbb@gmail.com>
2018-03-29 18:10:20 -0300

    Questao 6 e 7
    por Abraao

--- atividade1/main.py
+++ atividade1/main.py
@@ -18,11 +18,14 @@ def main():
 	# imageResult = questao1(img)
 	# imageResult = questao2(img, 'green')
 	# imageResult = questao3(img)
+	# imageResult = questao5(img, 2)
 	# imageResult = questao4(img, 100)
-	imageResult = questao5(img, 2)
+	# imageResult = questao6(img,500)
+	
+	imageResult = questao7(img)
 
 	# salvar arquivo transformado
-	cv2.imwrite('newImage.png',imageResult)
+	cv2.imwrite('newImage1.png',imageResult)
 
 def questao1(originalImage):
 
@@ -119,7 +122,111 @@ def questao5(originalImage, brightness):
 
 	return imageResult
 
+def questao6(img, n):
+	'''
+	calcula o limiar com determina N
+	Parametros:
+		img: Imagem a ser modificada
+		n :  e' o tamanho da mask	'''
+	imageResult = None
+	height = len(img)
+ 	width = len(img[0])
+ 
+ 	# Usar metodo pixel a pixel
+ 	newImage = []
+ 	for h in range(height):
+ 		newImage.append([])
+ 		for w in range(width):
+ 			newImage[-1].append(limiar(img[h][w][2], img[h][w][1], img[h][w][0],n))
 
+
+	return convertArrayToNumpy(newImage)
+
+def questao7(img):
+	'''
+	calcula o limiar pela media dos valores da imagem
+	Parametros:
+		img: Imagem a ser modificada
+	'''
+	imageResult = None
+	height = len(img)
+ 	width = len(img[0])
+	r = 0
+	g = 0
+	b = 0
+	count = 0
+ 	# Usar metodo pixel a pixel
+ 	for h in range(height):
+ 		for w in range(width):
+			r += img[h][w][2]
+			g += img[h][w][1]
+			b += img[h][w][0]
+			count+=1
+
+
+	limiarR = r / count
+	limiarG = g / count
+	limiarB = b / count
+ 	
+	newImage = []
+	for h in range(height):
+ 		newImage.append([])
+ 		for w in range(width):
+ 			newImage[-1].append(limiarMedia(img[h][w][2], img[h][w][1], img[h][w][0],limiarR,limiarG,limiarB))
+
+	return convertArrayToNumpy(newImage)
+
+def limiarMedia(b,g,r,limiarR,limiarG,limiarB):
+
+    '''
+	limiar e' igual media de valores da banda
+	
+	Parametros:
+		b: valor do azul
+		g: valor do verde
+		r: valor do vergmelho
+		limiarR: media dos valores vermelhos
+		limiarG: media dos valores verdes
+		limiarB: media dos valores azul
+    '''
+    
+    if r < limiarR:
+		r = 0
+    else:
+		r = 255
+    if g < limiarG:
+		g = 0
+    else:
+		g = 255
+    if b < limiarR:
+		b = 0
+    else:
+		b = 255
+            
+		return [trunc(b),trunc(g),trunc(r)]
+
+def limiar(b,g,r,n):
+    '''
+    Multiplica o brilho de uma imagem de acordo com o valor brightness dado
+
+	Parametros:
+		parameters : Dicionario {'brightness': int}
+    '''
+    
+    if r < n:
+		r = 0
+    else:
+		r = 255
+    if g < n:
+		g = 0
+    else:
+		g = 255
+    if b < n:
+		b = 0
+    else:
+		b = 255
+            
+		return [trunc(b),trunc(g),trunc(r)]
 
 def convertArrayToNumpy(array):
 	'''	
